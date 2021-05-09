2d5e815f36ea983fd31989dac8971a8fd16357ed
Thomas Minier <tminier01@gmail.com>
2021-05-09 16:49:35 +0200

    first release for testing

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const tc = require('@actions/tool-cache');
 
-try {
-  const githubToken = core.getInput('token');
-  const hdtTag = core.getInput('hdt-tag')
-  const octokit = github.getOctokit(githubToken)
+async function run() {
+  try {
+    const githubToken = core.getInput('token');
+    const hdtTag = core.getInput('hdt-tag')
+    const octokit = github.getOctokit(githubToken)
+  
+    const tagInfos = await octokit.repos.getReleaseByTag({
+      owner: 'rdfhdt',
+      repo: 'hdt-cpp',
+      tag: hdtTag
+    });
+  
+    console.log(tagInfos);
+  
+    const hdtZipPath = await tc.downloadTool(tagInfos.zipball_url);
+    const hdtExtractedFolder = await tc.extractZip(hdtZipPath, 'path/to/extract/to');
 
-  const tagInfos = await octokit.repos.getReleaseByTag({
-    owner: '',
-    repo: 'hdt-cpp',
-    tag: hdtTag
-  });
-
-  console.log(tagInfos);
-
-  const hdtZipPath = await tc.downloadTool(tagInfos.zipball_url);
-  const hdtExtractedFolder = await tc.extractZip(hdtZipPath, 'path/to/extract/to');
-} catch (error) {
-  core.setFailed(error.message);
+    console.log(hdtExtractedFolder);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
