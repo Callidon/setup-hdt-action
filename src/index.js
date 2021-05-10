43a2072922ea9429991b59236577291d35ea5c81
Thomas Minier <tminier01@gmail.com>
2021-05-10 10:54:26 +0200

    add caching

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const cache = require('@actions/cache')
 const core = require('@actions/core')
 const github = require('@actions/github')
 const tc = require('@actions/tool-cache')
@@ -7,6 +8,7 @@ async function run() {
   try {
     const githubToken = core.getInput('token')
     const hdtTag = core.getInput('hdt-tag')
+    let targetSourceLocation = core.getInput('source-path')
     const octokit = github.getOctokit(githubToken)
 
     core.info('Fetching release information from rdfhdt/hdt-cpp')
@@ -16,25 +18,32 @@ async function run() {
       tag: hdtTag
     })
 
-    core.startGroup(`Downloading HDT release ${hdtTag}`)
-    core.info(`Downloading release for tag ${tagInfos.data.tag_name}`)
-    // TODO use generic infos provided by GitHub for computing the zipball download link
-    const hdtZipPath = await tc.downloadTool(`https://codeload.github.com/rdfhdt/hdt-cpp/zip/${tagInfos.data.tag_name}`)
-
-    core.info(`Extracting HDT release ${hdtTag}`)
-
-    let targetLocation = `./${tagInfos.data.tag_name}`
-    // if required, use 'source-path' as the target for extrating the zipball,
-    if (core.getInput('source-path') !== 'null') {
-      targetLocation = core.getInput('source-path')
+    const sourcesCacheKey = `rdfhdt-hdt-cpp-sources-${tagInfos.data.tag_name}`
+    if (targetSourceLocation === 'null') {
+      targetSourceLocation = `./${tagInfos.data.tag_name}`
     }
+    const sourcesCachePaths = [path.resolve(targetSourceLocation)]
 
-    const hdtExtractedFolder = await tc.extractZip(hdtZipPath, targetLocation)
-    const sourcePath = path.resolve(hdtExtractedFolder)
-    core.info(`HDT source files successfully extracted to ${sourcePath}`)
-    core.endGroup()
-
-    core.setOutput('source-path', sourcePath)
+    // try to find the sources in the cache, otherwise download them
+    const existingCacheKey = await cache.restoreCache(sourcesCachePaths, sourcesCacheKey)
+    if (existingCacheKey === undefined) {
+      core.startGroup(`Downloading HDT release ${tagInfos.data.tag_name}`)
+      core.info(`Downloading release for tag ${tagInfos.data.tag_name}`)
+      // TODO use generic infos provided by GitHub for computing the zipball download link
+      const hdtZipPath = await tc.downloadTool(`https://codeload.github.com/rdfhdt/hdt-cpp/zip/${tagInfos.data.tag_name}`)
+  
+      core.info(`Extracting HDT release ${tagInfos.data.tag_name}`)
+  
+      const hdtExtractedFolder = await tc.extractZip(hdtZipPath, targetSourceLocation)
+      const sourcePath = path.resolve(hdtExtractedFolder)
+      core.info(`HDT source files successfully extracted to ${sourcePath}`)
+      core.endGroup()
+      core.setOutput('source-path', sourcePath)
+    } else {
+      core.info(`Found a cache entry for HDT version ${tagInfos.data.tag_name}, skipping download`)
+      core.info(`Restoring HDT version from cache to ${sourcesCachePaths[0]}`)
+      core.setOutput('source-path', sourcesCachePaths[0])
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
